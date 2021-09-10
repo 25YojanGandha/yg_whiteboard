199a96caab68c192cc95a934d3069df79d642d7e
Yojan Gandha <yojangandha1806@gmail.com>
2021-09-11 00:05:23 +0530

    Pencil Tool Added CSS Changes

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let canvas = document.querySelector(".canvas");
 let container = document.querySelector(".canvascontainer");
+let pencil = document.querySelector(".pencil");
+let eraser = document.querySelector(".eraser");
 
 canvas.width = container.clientWidth-5;
 canvas.height = container.clientHeight-5;
 
 let ctx = canvas.getContext("2d");
-let penselected = true;
 let pendown = false;
 
 canvas.addEventListener("mousedown", function(e){
+
     pendown = true;
-    let x = e.clientX;
-    let y = e.clientY;
+    let x = e.clientX - 55.2;
+    let y = e.clientY - 3*33.6;
     ctx.beginPath();
     ctx.moveTo(x,y);
 });
@@ -28,3 +30,5 @@ canvas.addEventListener("mousemove", function(e){
 canvas.addEventListener("mouseup", function(){
     pendown = false;
 });
+
+
